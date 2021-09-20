043027c4d214d306ad284fb341ec3c7075503e72
MB <matyok.boglarka@wm-iskola.hu>
2021-09-20 09:28:58 +0200

    1 feladat

---
+++ 20210920/20210920/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            if (a%2==0)
+            {
+                Console.WriteLine("A szám páros");
+            }
+            else
+            {
+                Console.WriteLine("A szám páratlan");
+            }
+            
+            Console.WriteLine("enter");
+            Console.ReadLine();
+        }
+    }
+}
