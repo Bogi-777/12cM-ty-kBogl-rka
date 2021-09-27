d1fd663df21ac9f434fd0e20b6d2bf9d4501c308
MB <matyok.boglarka@wm-iskola.hu>
2021-09-27 09:07:32 +0200

    1 feladat

---
+++ 20210927/20210927/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210927
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Írj be egy számot:");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            switch (szam)
+            {
+                case 1:
+                    Console.WriteLine("január");
+                    break;
+                case 2:
+                    Console.WriteLine("február");
+                    break;
+                case 3:
+                    Console.WriteLine("március");
+                    break;
+                case 4:
+                    Console.WriteLine("április");
+                    break;
+                case 5:
+                    Console.WriteLine("május");
+                    break;
+                case 6:
+                    Console.WriteLine("június");
+                    break;
+                case 7:
+                    Console.WriteLine("július");
+                    break;
+                case 8:
+                    Console.WriteLine("augusztus");
+                    break;
+                case 9:
+                    Console.WriteLine("szeptember");
+                    break;
+                case 10:
+                    Console.WriteLine("október");
+                    break;
+                case 11:
+                    Console.WriteLine("november");
+                    break;
+                case 12:
+                    Console.WriteLine("december");
+                    break;
+                default:
+                    Console.WriteLine("Nem hónap");
+                    break;
+            }
+
+
+            Console.WriteLine("enter");
+            Console.ReadLine();
+        }
+    }
+}
