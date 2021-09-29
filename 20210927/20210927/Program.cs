a5b0dd7db5c246a0cb2f7382d725b7c9d211dd90
MB <matyok.boglarka@wm-iskola.hu>
2021-09-29 12:28:08 +0200

    feladat

--- 20210927/20210927/Program.cs
+++ 20210927/20210927/Program.cs
@@ -11,51 +11,11 @@ namespace _20210927
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Írj be egy számot:");
-            int szam = Convert.ToInt32(Console.ReadLine());
-            switch (szam)
-            {
-                case 1:
-                    Console.WriteLine("tél");
-                    break;
-                case 2:
-                    Console.WriteLine("tél");
-                    break;
-                case 3:
-                    Console.WriteLine("tavasz");
-                    break;
-                case 4:
-                    Console.WriteLine("tavasz");
-                    break;
-                case 5:
-                    Console.WriteLine("tavasz");
-                    break;
-                case 6:
-                    Console.WriteLine("nyár");
-                    break;
-                case 7:
-                    Console.WriteLine("nyár");
-                    break;
-                case 8:
-                    Console.WriteLine("nyár");
-                    break;
-                case 9:
-                    Console.WriteLine("ősz");
-                    break;
-                case 10:
-                    Console.WriteLine("ősz");
-                    break;
-                case 11:
-                    Console.WriteLine("ősz");
-                    break;
-                case 12:
-                    Console.WriteLine("tél");
-                    break;
-                default:
-                    Console.WriteLine("Nincs ilyen hónap");
-                    break;
-            }
 
+            for (int i = 0; i < length; i++)
+            {
+
+            }
 
             Console.WriteLine("enter");
             Console.ReadLine();
